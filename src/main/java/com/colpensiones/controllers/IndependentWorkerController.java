1efd28c03b63e85947e34b5c5aee915f7c9a5b5f
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-30 11:53:28 -0500

    Correction in the not found exception

--- src/main/java/com/colpensiones/controllers/IndependentWorkerController.java
+++ src/main/java/com/colpensiones/controllers/IndependentWorkerController.java
@@ -2,7 +2,6 @@ package com.colpensiones.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.colpensiones.domain.exceptions.IndependentWorkerNotFoundException;
 import com.colpensiones.domain.models.IndependentWorker;
 import com.colpensiones.domain.repositories.IndependentWorkerRepository;
 
@@ -20,29 +20,24 @@ public class IndependentWorkerController {
 	IndependentWorkerController(IndependentWorkerRepository repository) {
 		this.repository = repository;
 	}
-	
-	  // Aggregate root
-	
-	  @GetMapping("/independent-worker")
-	  List<IndependentWorker> all() {
-	    return repository.findAll();
-	  }
-	
-	  @PostMapping("/independent-worker")
-	  IndependentWorker newIndependentWorker(@RequestBody IndependentWorker newWorker) {
-	    return repository.save(newWorker);
-	  }
 
-	  @PutMapping("/independent-worker/{document}")
-	  IndependentWorker replaceIndependentWorker(@RequestBody IndependentWorker modifiedWorker,@PathVariable Long document) {
-	
-	    return repository.findById(document)
-	      .map(worker -> {
-	    	  worker.setApproved(modifiedWorker.isApproved());
-	        return repository.save(worker);
-	      })
-	      .orElseGet(() -> {
-	        return null;
-	      });
-	  }
+	@GetMapping("/independent-worker")
+	List<IndependentWorker> all() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/independent-worker")
+	IndependentWorker newIndependentWorker(@RequestBody IndependentWorker newWorker) {
+		return repository.save(newWorker);
+	}
+
+	@PutMapping("/independent-worker/{document}")
+	IndependentWorker replaceIndependentWorker(@RequestBody IndependentWorker modifiedWorker,
+			@PathVariable Long document) {
+
+		return repository.findById(document).map(worker -> {
+			worker.setApproved(modifiedWorker.isApproved());
+			return repository.save(worker);
+		}).orElseThrow(() -> new IndependentWorkerNotFoundException(document));
+	}
 }
--- src/main/java/com/colpensiones/domain/exceptions/IndependentWorkerNotFoundException.java
+++ src/main/java/com/colpensiones/domain/exceptions/IndependentWorkerNotFoundException.java
@@ -2,7 +2,7 @@ package com.colpensiones.domain.exceptions;
 
 public class IndependentWorkerNotFoundException extends RuntimeException {
 
-	IndependentWorkerNotFoundException(Long document) {
+	public IndependentWorkerNotFoundException(Long document) {
 		super("El trabajador con document " + document + " no fue encontrado.");
 	}
 }
