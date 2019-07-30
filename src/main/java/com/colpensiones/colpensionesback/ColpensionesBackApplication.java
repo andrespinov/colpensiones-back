fa76b95bd1211399e8b5bd34caddc7bd479c1d36
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-30 09:34:15 -0500

    Project generated

---
+++ src/main/java/com/colpensiones/colpensionesback/ColpensionesBackApplication.java
@@ -0,0 +1,13 @@
+package com.colpensiones.colpensionesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColpensionesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColpensionesBackApplication.class, args);
+	}
+
+}
